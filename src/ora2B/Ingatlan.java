8efa6046bedebda27b1140f1e0842cd573fa570d
Sándor Vályi <urugvalyi@gmail.com>
2025-04-14 18:29:09 +0200

    febr28 végállapot

--- src/ora2B/Ingatlan.java
+++ src/ora2B/Ingatlan.java
@@ -33,9 +33,14 @@ public class Ingatlan implements IngatlanInterface {
     }
 
     public static void main(String[] a) {
-        Ingatlan ing1 = new Ingatlan("Debrecen",1000000.0,10,2.5,Tipus.TARSASHAZ);
-        ing1.akcio(0);
-        System.out.println(ing1.toString());
+        Ingatlan ing1 = new Ingatlan("Debrecen",1000000.0,100,2.5,Tipus.TARSASHAZ);
+        Panel pan1 = new Panel("Debrecen",800000.0,100,4,Tipus.TARSASHAZ,4,false);
+        ing1.akcio(20);
+        pan1.akcio(0);
+        System.out.println(ing1);
+        System.out.println(pan1);
+        System.out.println("ugyannayi-e? "+pan1.ugyanannyi(ing1));
+
     }
 
     @Override
